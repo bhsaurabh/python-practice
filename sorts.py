d8a01cf949144af44129f8a1ef5637d4674bc06f
Saurabh Bhatia <saurabh.bhatia@me.com>
2014-06-22 15:32:04 +0000

    Short bubble sort

--- sorts.py
+++ sorts.py
@@ -2,13 +2,14 @@
 
 def bubble(a_list):
     """
-    Sorts a list using bubble sort
+    Sorts a list using short-bubble sort
     """
-    for pass_number in range(len(a_list)-1, 0, -1):
-        # pass number approach is more efficient, as with every iteration
-        # largest element is in position
+    exchanges = True
+    pass_number = len(a_list) - 1  # number of passes
+    while pass_number > 0 and exchanges:
+        exchanges = False  # proceed only if there are exchanges made
         for i in range(pass_number):
             if a_list[i] > a_list[i+1]:
+                exchanges = True
                 # exchange
                 a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
-                
