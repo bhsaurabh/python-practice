177b027820ef8504198852cc168268eab34d1447
Saurabh Bhatia <saurabh.bhatia@me.com>
2014-06-22 15:55:16 +0000

    Highly efficient insertion sort

--- sorts.py
+++ sorts.py
@@ -18,6 +18,7 @@ def bubble(a_list):
 def selection(a_list):
     """
     Sorts a list using selection sort
+    O(N^2) compares and O(N) exchanges
     """
     # find minimum and put it in position
     N = len(a_list)
@@ -27,4 +28,17 @@ def selection(a_list):
             if a_list[j] < a_list[min]:
                 min = j
         a_list[i], a_list[min] = a_list[min], a_list[i]
-    
+
+def insertion(a_list):
+    """
+    Sorts a list using insertion sort
+    """
+    for i in range(len(a_list)):
+        elem = a_list[i]
+        for j in range(i, 0, -1):
+            if elem < a_list[j-1]:
+                # shift this position, shifts are a third as expensive as exchanges
+                a_list[j] = a_list[j-1]
+            else:
+                a_list[j] = elem
+                break  # stop more iterations
